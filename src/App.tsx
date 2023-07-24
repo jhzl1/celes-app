a1f01e9bf94118440f635b44b0ebc23627256461
Omar Campos <omarjc-1997@hotmail.com>
2023-07-24 15:13:44 -0500

    assets configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import { useState } from "react"
-import reactLogo from "./assets/react.svg"
-import viteLogo from "/vite.svg"
-import "./App.css"
 import { Login } from "pages"
+import { Button } from "components/Button"
 
 export const App = () => {
   const [count, setCount] = useState(0)
@@ -11,12 +9,8 @@ export const App = () => {
     <>
       <Login />
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <a href="https://vitejs.dev" target="_blank"></a>
+        <a href="https://react.dev" target="_blank"></a>
       </div>
       <h1>Vite + React</h1>
       <div className="card">
@@ -25,6 +19,8 @@ export const App = () => {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
+
+      <Button />
       <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
     </>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#1b1e30",
+        secondary: "#087eff",
+      },
+    },
   },
   plugins: [],
 }
